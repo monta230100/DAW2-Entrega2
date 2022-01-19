7c10b769c0eaa2ef29da787b675adb0a0b8a56dc
Cristian Montañés <cmontaes@everis.com>
2022-01-19 23:51:59 +0100

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'slider-principal',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'pagina-principal',
+    loadChildren: () => import('./components/pagina-principal/pagina-principal.module').then( m => m.PaginaPrincipalPageModule)
+  },
+  {
+    path: 'slider-principal',
+    loadChildren: () => import('./components/slider-principal/slider-principal.module').then( m => m.SliderPrincipalPageModule)
+  },
+  {
+    path: 'formulari',
+    loadChildren: () => import('./components/formulari/formulari.module').then( m => m.FormulariPageModule)
+  },
+  {
+    path: 'llistat',
+    loadChildren: () => import('./components/llistat/llistat.module').then( m => m.LlistatPageModule)
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './components/components.module';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ComponentsModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
